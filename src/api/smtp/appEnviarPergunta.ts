4f0031670c29fa53d80e46eb0396904c5174be9a
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-03-09 15:00:15 -0300

    passed the right function

--- src/api/smtp/appEnviarPergunta.ts
+++ src/api/smtp/appEnviarPergunta.ts
@@ -4,7 +4,7 @@ import SmtpService from '../../services/smtpService';
 export default async (req, res, next) => {
   try {
 
-    const payload = await new SmtpService(req).enviarVerificacao(req.body.nome, req.body.email, req.body.telefone, emailContent);
+    const payload = await new SmtpService(req).enviarPergunta(req.body.nome, req.body.email, req.body.telefone, req.body.emailContent);
 
     await ApiResponseHandler.success(req, res, payload);
   } catch (error) {
