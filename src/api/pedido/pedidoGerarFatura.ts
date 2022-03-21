ed1eb06a796cb6a94b506deb4b842144c7da27ea
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-03-21 12:54:53 -0300

    now the user cpf is used

--- src/api/pedido/pedidoGerarFatura.ts
+++ src/api/pedido/pedidoGerarFatura.ts
@@ -10,7 +10,7 @@ export default async (req, res, next) => {
     );
 
     const payload = await new PedidoService(req).geraFatura(
-      req.params.id
+      req.params.id, req.body.data
     );
 
     await ApiResponseHandler.success(req, res, payload);
